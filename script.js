f1c4c44ac0f5632edb0cb22fac48217c5d7239a2
pthngws <pthngws@gmail.com>
2024-12-26 14:09:05 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hiệu ứng cuộn
+    const links = document.querySelectorAll("a[href^='#']");
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
